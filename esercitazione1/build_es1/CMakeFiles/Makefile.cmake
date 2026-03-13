11ea242c86df658dfe48c1baf985d8ce5f7efc76
Luca Balliano <s325593@studenti.polito.it>
2026-03-13 08:27:06 +0000

    Correzione CMakeLists.txt per compilare anche Printargs

--- esercitazione1/build_es1/CMakeFiles/Makefile.cmake
+++ esercitazione1/build_es1/CMakeFiles/Makefile.cmake
@@ -137,5 +137,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/Printargs.dir/DependInfo.cmake"
   "CMakeFiles/Esercizio.dir/DependInfo.cmake"
   )
---
+++ esercitazione1/build_es1/CMakeFiles/Printargs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Printargs.dir/link.d"
+  "CMakeFiles/Printargs.dir/Printargs.cpp.o"
+  "CMakeFiles/Printargs.dir/Printargs.cpp.o.d"
+  "Printargs"
+  "Printargs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Printargs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
